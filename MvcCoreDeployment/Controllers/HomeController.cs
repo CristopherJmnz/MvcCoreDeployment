9c1686b75c921063569b192b575a91f12348a7f9
CristopherJmnz <crisjimenez19@gmail.com>
2024-04-29 11:50:07 +0200

    Add project files.

---
+++ MvcCoreDeployment/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDeployment.Models;
+using System.Diagnostics;
+
+namespace MvcCoreDeployment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Web deployment de lues";
+            List<int>nums= new List<int>(); 
+            for (int i = 0; i < 11; i++)
+            {
+                nums.Add(i);
+            }
+            return View(nums);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
